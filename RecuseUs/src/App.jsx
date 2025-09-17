a373e0fc40d3a0f9584d417c9024c880e20c92ec
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-09-17 17:04:02 -0300

    Iniciando códigos NA MAIN

--- RecuseUs/src/App.jsx
+++ RecuseUs/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import CardRecursos from './components/CardRecursos'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='align-items-center'>
+
+      <h2>Recursos Essenciais</h2>
+      <p >Ferramentas desenvolvidas especialmente para as necessidades dos
+      bombeiros em campo</p>
+      <CardRecursos
+      />
+      <h2>Recursos Essenciais</h2>
+      <p>Veja o que os bombeiros de todo o Brasil estão dizendo.</p>
+
+    </div>
   )
 }
 
